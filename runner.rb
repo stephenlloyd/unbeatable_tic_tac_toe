f29dcde46f544edac37cdc2564dbe0970a5eace3
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2015-03-06 15:11:03 +0000

    runner should work now

--- runner.rb
+++ runner.rb
@@ -4,7 +4,7 @@ require 'terminal-table'
 print "Welcome to Tic Tac Toe\n please enter your name:\n>:"
 name = gets.chomp
 
-game = TicTacToe.new(name)
+game = TicTacToe.new(name, Array.new(9))
 
 puts  Terminal::Table.new :rows => game.grid.each_slice(3).to_a
 puts "please put the index (0 indexed) of the square you want to hit\n or type 'exit' to exit"
